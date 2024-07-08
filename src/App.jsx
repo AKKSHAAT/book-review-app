71e9059bd75922263a552a8c6a13590f556a1c49
AKKSHAAT <akkshaat@gmail.com>
2024-07-08 19:35:30 +0530

    added: login forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './pages/Login'
+import { Registration } from './pages/Registration'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={ <Login /> }/>
+    
+      <Route path='/register' element={ <Registration /> }/>
+      <Route path='/login' element={ <Login /> }/>
     </Routes>
   )
 }
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -2,38 +2,41 @@ import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
-export const Login = () => {
+export const Registration = () => {
     const navigate = useNavigate();
 
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
     const handleUsernameChange = (e) => setUsername(e.target.value);
+    const handleEmailChange = (e) => setEmail(e.target.value);
     const handlePasswordChange = (e) => setPassword(e.target.value);
 
 
-    function loginUser(){
+    function registerUser(){
         if(username && email && password) {
             console.log('works');
         }
     }
 
-    function takeToRegister(){
+    function takeTOLogin(){
         console.log("clicked");
-        navigate("/");
+        navigate("/login");
     }
 
   return (
     <div className='bg-white w-full h-[70vh] rounded-xl shadow-md flex flex-col items-center p-10'>
-        <h1 className='text-xl font-semibold'>Login</h1>
+        <h1 className='text-xl font-semibold'>Register</h1>
         
         <div className='m-2 text-lg'>
+            <input className='w-[70vw] h-[2.5rem] rounded-lg border-2 border-gray-800 p-4 m-2' type='email' onChange={handleEmailChange} placeholder='email'></input>
             <input className='w-[70vw] h-[2.5rem] rounded-lg border-2 border-gray-800 p-4 m-2' type='text' onChange={handleUsernameChange} placeholder='Username'></input>
             <input className='w-[70vw] h-[2.5rem] rounded-lg border-2 border-gray-800 p-4 m-2' type='password' onChange={handlePasswordChange} placeholder='password'></input>
         </div>
 
-          <button onClick={loginUser} className='text-white text-lg bg-succ py-2 px-4 rounded-lg font-semibold m-2'>Login</button>
-          <button onClick={takeToRegister} className='text-white text-lg bg-succ py-2 px-4 rounded-lg font-semibold m-2'>Register Me</button>
+            <button onClick={registerUser} className='text-white text-lg bg-succ py-2 px-4 rounded-lg font-semibold m-2'>Register Me</button>
+            <button onClick={takeTOLogin} className='text-white text-lg bg-succ py-2 px-4 rounded-lg font-semibold m-2'>Login</button>
         
 
 
